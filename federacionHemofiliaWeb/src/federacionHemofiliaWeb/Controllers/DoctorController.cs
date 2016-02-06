f402032c469a9cb16042958e4a4ad6365a58ed87
Miguel Pérez García <miguelpg_95@hotmail.com>
2016-02-06 17:45:54 -0600

    agregando el view para el doctor

---
+++ federacionHemofiliaWeb/src/federacionHemofiliaWeb/Controllers/DoctorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace federacionHemofiliaWeb.Controllers
+{
+    public class ManageController : Controller
+    {
+        [HttpGet]
+        public IActionResult Paciente()
+        {
+            return View();
+        }
+    }
+}
