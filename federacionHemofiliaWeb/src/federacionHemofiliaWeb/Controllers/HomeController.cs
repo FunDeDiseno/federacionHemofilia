7943354a02bd1b075600e3839d4db3b89e8d584a
Marcko45 <marcko.rp@gmail.com>
2016-03-08 14:21:56 -0600

    Acerca html (falta mejorar), abre pagina desde botones de Index en el carousel, NO ABRE EL LINK (ya esta en el controller de Home...)

--- federacionHemofiliaWeb/src/federacionHemofiliaWeb/Controllers/HomeController.cs
+++ federacionHemofiliaWeb/src/federacionHemofiliaWeb/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace federacionHemofiliaWeb.Controllers
         {
             return View();
         }
+        
+        public IActionResult Acerca()
+        {
+            return View();
+        }
     }
 }
